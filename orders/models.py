6dbe7ee1f50d1c702133d86b49dd84d8e8fa541e
wyc1997 <home@wuyangchaodeMacBook-Air.local>
2018-08-20 11:33:11 +0800

    half complete order list

--- orders/models.py
+++ orders/models.py
@@ -21,21 +21,21 @@ class Sub(models.Model):
     price = models.DecimalField(decimal_places = 2, max_digits = 64)
 
     def __str__(self):
-        return f"{self.size} {self.name}"
+        return f"{self.size}_{self.name}".replace(" ", "_")
 
 class Pasta(models.Model):
     name = models.CharField(max_length = 64)
     price = models.DecimalField(decimal_places = 2, max_digits = 64)
 
     def __str__(self):
-        return f"{self.name}"
+        return f"{self.name}".replace(" ", "_").replace("/", "_")
 
 class Salad(models.Model):
     name = models.CharField(max_length = 64)
     price = models.DecimalField(decimal_places = 2, max_digits = 64)
 
     def __str__(self):
-        return f"{self.name}"
+        return f"{self.name}".replace(" ", "_")
 
 class Dinner_Platter(models.Model):
     name = models.CharField(max_length = 64)
@@ -43,7 +43,7 @@ class Dinner_Platter(models.Model):
     price = models.DecimalField(decimal_places = 2, max_digits = 64)
 
     def __str__(self):
-        return f"{self.size} {self.name}"
+        return f"{self.size}_{self.name}".replace(" ", "_")
 
 class Order(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
--- orders/static/js/index.js
+++ orders/static/js/index.js
@@ -5,11 +5,13 @@ var pasta_data;
 var salad_data;
 var dinner_platter_data;
 var topping_data;
+var num_item = 1;
+var total_price = 0.0
 
 document.addEventListener("DOMContentLoaded", () => {
     populate_table(presented);
 
-})
+});
 
 
 document.querySelectorAll(".td_left").forEach(td => {
@@ -21,7 +23,9 @@ document.querySelectorAll(".td_left").forEach(td => {
         presented = td.id;
         populate_table(presented);
     }
-})
+});
+
+
 
 function populate_table(presented) {
     var mid_table = document.querySelector("#mid_table")
@@ -67,4 +71,19 @@ function populate_table(presented) {
             mid_table.innerHTML = document.querySelector("#sub_tem").innerHTML
         break;
     }
-}
+    document.querySelectorAll(".add_order_butt").forEach(button => {
+        button.onclick = () => {
+            var data = document.querySelectorAll(`.${button.id}`);
+            document.querySelector("#ver_table_right").innerHTML += document.querySelector("#order_list_item_1").innerHTML;
+            document.querySelector("#order_list_span_name").innerHTML = data[0].innerHTML;
+            document.querySelector("#order_list_span_price").innerHTML = data[1].innerHTML;
+            document.querySelector("#order_list_span_name").id = `order_list_span_name_${num_item}`;
+            document.querySelector("#order_list_span_price").id = `order_list_span_price_${num_item}`;
+            document.querySelector("#tr_0").id = `tr_${num_item}`;
+            document.querySelector("#total_item").innerHTML = num_item;
+            total_price += parseFloat(data[1].innerHTML.substring(1));
+            document.querySelector("#total_price").innerHTML = total_price;
+            num_item++;
+        }
+    })
+};
